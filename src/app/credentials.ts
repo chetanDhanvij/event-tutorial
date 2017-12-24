bf4bb16a66521b30d3d82f5f17c7370b2ad946b5
Jorge Vergara <javebratt@gmail.com>
2017-12-24 06:12:24 -0500

    Adds credentials.ts to the .gitignore file for better practices

--- src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCDoh6pZ1JroJbb6rUHpKKHmHl3Sigw2GE',
-  authDomain: 'event-manager-tutorial.firebaseapp.com',
-  databaseURL: 'https://event-manager-tutorial.firebaseio.com',
-  projectId: 'event-manager-tutorial',
-  storageBucket: 'event-manager-tutorial.appspot.com',
-  messagingSenderId: '468364937162'
-};
